c62d4f3e6159af64e095a26b1c4b93cc1426795d
Carmen Sanchez <carmenmasato@gmail.com>
2020-02-24 09:48:09 -0400

    proyecto

---
+++ ViajesTuristicos/Controllers/HelperController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViajesTuristicos.Models;
+
+namespace ViajesTuristicos.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Destino(Cliente cliente)
+        {
+            if(cliente.Destino == Dest.Playa)
+            {
+                cliente.pic = "/img/playa.jpg";
+            }
+            if (cliente.Destino == Dest.Montaña)
+            {
+                cliente.pic = "/img/montana.jpg";
+            }
+            if (cliente.Destino == Dest.Europa)
+            {
+                cliente.pic = "/img/europa.jpg";
+            }
+            if (cliente.Destino == Dest.USA)
+            {
+                cliente.pic = "/img/usa.jpg";
+            }
+            return View(cliente);
+        }
+    }
+}
---
+++ ViajesTuristicos/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViajesTuristicos.Models
+{
+    public class Cliente
+    {
+
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public Dest Destino { get; set; }
+        public string pic { get; set; }
+    }
+
+    public enum Dest
+    {
+        Playa,Montaña,USA,Europa
+    }
+}
